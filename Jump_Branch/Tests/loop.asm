fcc2ec99c60ac66d0d135bce81601b73cda178ad
Fiona Shyne <shynef@union.edu>
2022-03-14 17:46:46 -0400

    updated tests
    Had tests use register labels

--- Jump_Branch/Tests/loop.asm
+++ Jump_Branch/Tests/loop.asm
@@ -1,11 +1,11 @@
 # add first four values in memory
-# $1: length, $2: i, $3 sum, $4 temp 
-addi $1 $0 4 
-addi $2 $0 0
-addi $3 $0 0 
+# $s0: length, $s1: i, $s2 sum, $s3 temp 
+addi $s0 $0 4 
+addi $s1 $0 0
+addi $s2 $0 0 
 
-beq $1 $2 5  
-lw $4 0($2)
-add $3 $3 $4 
-addi $2 $2 1 
+beq $s0 $s1 5  
+lw $s3 0($s1)
+add $s2 $s2 $s3 
+addi $s1 $s1 1 
 j 3 
--- Jump_branch/Tests/reverse.asm
+++ Jump_branch/Tests/reverse.asm
@@ -1,15 +1,15 @@
-Addi $1 $0 5 # $1 contains the last element  
-Lw $2 0($0)  
-LW $3 0($1) # load the first and last elements into $2 and $3 
-Sw $2 0($1) 
-SW $3 0($0) # swap first and last 
+Addi $t0 $0 5 # $t0 contains the last element  
+Lw $t1 0($0)  
+LW $t2 0($t0) # load the first and last elements into $t1 and $t2 
+Sw $t1 0($t0) 
+SW $t2 0($0) # swap first and last 
 
-Lw $2 1($0)  
-LW $3 -1($1) 
-Sw $2 -1($1) 
-SW $3 1($0)  
+Lw $t1 1($0)  
+LW $t2 -1($t0) 
+Sw $t1 -1($t0) 
+SW $t2 1($0)  
 
-Lw $2 2($0) # repeat with second first/last 
-LW $3 -2($1) 
-Sw $2 -2($1)  
-SW $3 2($0) # swap 3rd and 4th  
+Lw $t1 2($0) # repeat with second first/last 
+LW $t2 -2($t0) 
+Sw $t1 -2($t0)  
+SW $t2 2($0) # swap 3rd and 4th  
